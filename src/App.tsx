4c9eeaa0eda9d1876d333a1cb81dd84cd9589bfe
derickcjesus <derickcjesus@gmail.com>
2022-06-25 10:27:35 -0300

    testando conexão com a api usando apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+
+ const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+ `
+
+ interface Lesson {
+  id: string;
+  title: string;
+ }
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
+
+  console.log(data)
+ 
   return (
-    <h1 className="text-5xl text-violet-700">Jesus is Lord</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li className="text-red-800">{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4stesoq2jco01yygwqbhb6p/master',
+    cache: new InMemoryCache()
+})
